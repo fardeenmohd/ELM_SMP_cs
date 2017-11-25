b25aef8d9688fbdb86d53cae1e6b6f5833685acb
mateusz <mateusz@COMMANDERLAMP>
2017-11-25 19:54:21 +0100

    init

---
+++ ELM_SMP/ELM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace ELM_SMP
+{
+    class ELM
+    {
+        int nInputs;
+        int nHidden;
+        Matrix<double> IW;
+        Matrix<double> Betha;
+        Matrix<double> H;
+        //actfun (1./(1+exp(-x)))
+        int bias;
+
+        public ELM(int nInputs,int nHidden,int bias)
+        {
+            this.nInputs = nInputs;
+            this.nHidden = nHidden;
+            this.bias = bias;
+
+        }
+
+        public void train(Matrix<double> X, Matrix<double> Y)
+        {
+            //X =;
+//            X = [(ones(size(X, 1), 1) * obj.bias) X];
+//            obj.IW = randInitializeWeights(obj.nInputs, obj.nHidden);
+//            obj.H = X * obj.IW';
+//obj.Betha = pinv(obj.H) * Y;
+        }
+        public void predict(Matrix<double> X)
+        {
+            //X = [(ones(size(X, 1), 1) * obj.bias) X];
+            //Hi = X * obj.IW';
+            //Y = Hi * obj.Betha;
+        }
+    }
+}
---
+++ ELM_SMP/Form1.cs
@@ -0,0 +1,37 @@
+﻿using MathNet.Numerics.Data.Text;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ELM_SMP
+{
+    public partial class Form1 : Form
+    {
+        Matrix<double> data;// = DelimitedReader.Read<double>("data.csv", false, ",", true);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openCSV_btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofile = new OpenFileDialog();
+            ofile.Filter = "CSV Files (*.csv)|*.csv";
+            if (DialogResult.OK == ofile.ShowDialog())
+            {
+                data = DelimitedReader.Read<double>(ofile.FileName, false, ",", true, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
+                var ci = System.Globalization.CultureInfo.CurrentCulture.NumberFormat;
+                int a = 5;
+             
+            }
+        }
+    }
+}
